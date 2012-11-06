524288ef1017dbb61b39f77a2757cba288b8f36a
omar <elgordo18635985@gmail.com>
2012-11-06 01:45:43 -0430

    AlphaBetaPruning Working :)

--- othello_Alpha_Beta_Pruning.h
+++ othello_Alpha_Beta_Pruning.h
@@ -1,37 +1,37 @@
 int alpha_beta_pruning(state_t state, int d, int alpha, int beta, bool Player) {
-    if (state.terminal() || d == 0) {
+    if (state.terminal()) {
         //std::cout<<"hola1";
         return state.value();
     }
-    bool existOne=false;
+    bool existOne = false;
 
     if (Player) {
         //gather possible moves for succersors
         for (int pos = 0; pos < DIM; ++pos) {
             if ((state.is_black_move(pos))) {
-                existOne=true;
+                existOne = true;
                 alpha = std::max(alpha, alpha_beta_pruning(state.move(1, pos), d - 1, alpha, beta, 0));
                 if (alpha >= beta) {
                     break;
                 }
-            }     
+            }
         }
-        if(!existOne){
-            alpha =std::max(alpha,alpha_beta_pruning(state, d - 1, alpha, beta, 0));
+        if (!existOne) {
+            alpha = std::max(alpha, alpha_beta_pruning(state, d - 1, alpha, beta, 0));
         }
         return alpha;
     } else {
         //gather possible moves for succersors
         for (int pos = 0; pos < DIM; ++pos) {
             if ((state.is_white_move(pos))) {
-                existOne=true;
+                existOne = true;
                 beta = std::min(beta, alpha_beta_pruning(state.move(0, pos), d - 1, alpha, beta, 1));
                 if (alpha >= beta) {
                     break;
                 }
             }
         }
-        if(!existOne){
+        if (!existOne) {
             beta = std::min(beta, alpha_beta_pruning(state, d - 1, alpha, beta, 1));
         }
         return beta;
@@ -39,51 +39,82 @@ int alpha_beta_pruning(state_t state, int d, int alpha, int beta, bool Player) {
 }
 
 int alpha_beta_pruning_TT(state_t state, int d, int alpha, int beta, bool Player) {
-    
-    hash_table_t::iterator it = dd.find(state);
-    if (it != dd.end()) {
-        return getValue(it->second);
-    }
-    
-    if (state.terminal() || d == 0) {
-        //std::cout<<"hola1";
-        insert_hash(state, beta);        
+
+    if (state.terminal()) {
+        //insert_hash(state, state.value(), 0);
         return state.value();
     }
-    
-    bool existOne=false;
+
+    hash_table_t::iterator it = dd.find(state);
+    if (it != dd.end()) {
+        if ((it->second).option == 0) {
+            return getValue(it->second);
+        }
+        if (((it->second).option == 1) && Player) {
+            if (alpha < getValue(it->second)) {
+                alpha = getValue(it->second);
+            }
+        }
+        if (((it->second).option == 2) && !Player) {
+            if (beta > getValue(it->second)) {
+                beta = getValue(it->second);
+            }
+        }
+    }
+
+    bool existOne = false;
 
     if (Player) {
         //gather possible moves for succersors
         for (int pos = 0; pos < DIM; ++pos) {
             if ((state.is_black_move(pos))) {
-                existOne=true;
-                alpha = std::max(alpha, alpha_beta_pruning_TT(state.move(1, pos), d - 1, alpha, beta, 0));
-                if (alpha >= beta) {
-                    break;
+                existOne = true;
+                int abp = alpha_beta_pruning_TT(state.move(1, pos), d - 1, alpha, beta, 0);
+                if (alpha < abp && abp < beta) {
+                    insert_hash(state.move(1, pos), abp, 0);
                 }
-            }     
-        }
-        if(!existOne){
-            alpha =std::max(alpha,alpha_beta_pruning(state, d - 1, alpha, beta, 0));
-        }
-        insert_hash(state, beta);
-        return alpha;
-    } else {
-        //gather possible moves for succersors
-        for (int pos = 0; pos < DIM; ++pos) {
-            if ((state.is_white_move(pos))) {
-                existOne=true;
-                beta = std::min(beta, alpha_beta_pruning_TT(state.move(0, pos), d - 1, alpha, beta, 1));
+                if (abp < alpha) {
+                    insert_hash(state.move(1, pos), abp, 2);
+                }
+                if (beta < abp) {
+                    insert_hash(state.move(1, pos), abp, 1);
+                }
+                alpha = std::max(alpha, abp);
                 if (alpha >= beta) {
                     break;
                 }
             }
         }
-        if(!existOne){
+        if (!existOne) {
+            alpha = std::max(alpha, alpha_beta_pruning(state, d - 1, alpha, beta, 0));
+        }
+        return alpha;
+    } else {
+        //gather possible moves for succersors
+        for (int pos = 0; pos < DIM; ++pos) {
+            if ((state.is_white_move(pos))) {
+                existOne = true;
+                int abp = alpha_beta_pruning_TT(state.move(0, pos), d - 1, alpha, beta, 1);
+
+                if (alpha < abp && abp < beta) {
+                    insert_hash(state.move(0, pos), abp, 0);
+                }
+                if (abp < alpha) {
+                    insert_hash(state.move(0, pos), abp, 2);
+                }
+                if (beta < abp) {
+                    insert_hash(state.move(0, pos), abp, 1);
+                }
+
+                beta = std::min(beta, abp);
+                if (alpha >= beta) {
+                    break;
+                }
+            }
+        }
+        if (!existOne) {
             beta = std::min(beta, alpha_beta_pruning(state, d - 1, alpha, beta, 1));
         }
-        insert_hash(state, beta);
         return beta;
     }
 }
